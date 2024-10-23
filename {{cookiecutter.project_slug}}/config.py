9e5c0b2870c353455d2d89478487ff685d3586a9
AlbinAimleap <albin.a@aimleap.com>
2024-10-23 13:35:31 +0530

    Fix mkdir issue for output dir

--- {{cookiecutter.project_slug}}/config.py
+++ {{cookiecutter.project_slug}}/config.py
@@ -4,7 +4,7 @@ CURRENT_DIR = Path(__file__)
 
 output_dir = CURRENT_DIR / "output"
 if not output_dir.exists():
-    output_dir.mkdir(parents=True)
+    output_dir.mkdir(parents=True, exist_ok=True)
 
 class Config:
     OUTPUT_FILE = output_dir / "output.json"
